c52afbd22712b5f5b093f526c7133077acd02d03
André Luis Fernandes <62942316+Andre-StudioNerd@users.noreply.github.com>
2025-07-17 15:10:27 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ import autores from "./autoresRoutes.js";
 const routes = (app) => {
   app
     .route("/")
-    .get((req, res) => res.status(200).send("Curso de Node.js- Atualizado"));
+    .get((req, res) => res.status(200).send("Curso de Node.js"));
 
   app.use(express.json(), livros, autores);
 };
